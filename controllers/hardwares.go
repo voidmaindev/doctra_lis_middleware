39e19fd7e04374882a4a21e8c48694d1cd21ab20
George Sabanadze <george.sabanadze@doctra.ge>
2024-04-15 19:16:28 +0400

    Added Swagger for hardwares CRUD

--- controllers/hardwares.go
+++ controllers/hardwares.go
@@ -9,73 +9,111 @@ import (
 	"github.com/voidmaindev/doctra_lis_middleware/store"
 )
 
+// @Summary Get all hardwares
+// @Description Get all hardwares
+// @Tags Hardwares
+// @Accept json
+// @Produce json
+// @Success 200 {object} fiber.Map
+// @Failure 500 {object} fiber.Map
+// @Router /hardwares [get]
 func HardwaresGetAll(c *fiber.Ctx) error {
 	hardwares := &[]models.Hardware{}
 	res := store.DB.Find(hardwares)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": res.Error.Error()})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": res.Error.Error()})
 		return nil
 	}
 
-	c.Status(http.StatusOK).JSON(&fiber.Map{"success" : true, "message": "All hardwares", "hardwares": hardwares})
+	c.Status(http.StatusOK).JSON(&fiber.Map{"success": true, "message": "All hardwares", "hardwares": hardwares})
 
 	return nil
 }
 
+// @Summary Get hardware by ID
+// @Description Get hardware by ID
+// @Tags Hardwares
+// @Accept json
+// @Produce json
+// @Param id path int true "Hardware ID"
+// @Success 200 {object} fiber.Map
+// @Failure 500 {object} fiber.Map
+// @Router /hardwares/{id} [get]
 func HardwaresGetByID(c *fiber.Ctx) error {
-	id := c.Params("id");
+	id := c.Params("id")
 	if id == "" {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "No ID entered"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "No ID entered"})
 		return nil
 	}
-	
+
 	hardware := &models.Hardware{}
 	res := store.DB.First(hardware, id)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "Record not found"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "Record not found"})
 		return nil
 	}
 
-	c.Status(http.StatusOK).JSON(&fiber.Map{"success" : true, "message": "Hardware", "hardware": hardware})
+	c.Status(http.StatusOK).JSON(&fiber.Map{"success": true, "message": "Hardware", "hardware": hardware})
 
 	return nil
 }
 
+// @Summary Create a new hardware
+// @Description Create a new hardware
+// @Tags Hardwares
+// @Accept json
+// @Produce json
+// @Param hardware body models.Hardware true "Hardware object"
+// @Success 200 {object} fiber.Map
+// @Failure 400 {object} fiber.Map
+// @Failure 500 {object} fiber.Map
+// @Router /hardwares [post]
 func HardwaresCreate(c *fiber.Ctx) error {
 	hardware := &models.Hardware{}
 	err := c.BodyParser(hardware)
 	if err != nil {
-		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success" : false, "message": err.Error()})
+		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success": false, "message": err.Error()})
 		return nil
 	}
 
 	res := store.DB.Create(hardware)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": res.Error.Error()})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": res.Error.Error()})
 		return nil
 	}
 
-	c.Status(http.StatusOK).JSON(&fiber.Map{"success" : true, "message": "Created", "hardware": hardware})
+	c.Status(http.StatusOK).JSON(&fiber.Map{"success": true, "message": "Created", "hardware": hardware})
 
 	return nil
 }
 
+// @Summary Update hardware by ID
+// @Description Update hardware by ID
+// @Tags Hardwares
+// @Accept json
+// @Produce json
+// @Param id path int true "Hardware ID"
+// @Param hardware body models.Hardware true "Updated hardware object"
+// @Success 200 {object} fiber.Map
+// @Failure 400 {object} fiber.Map
+// @Failure 500 {object} fiber.Map
+// @Router /hardwares/{id} [put]
 func HardwaresUpdate(c *fiber.Ctx) error {
-	idStr := c.Params("id");
+	idStr := c.Params("id")
 	if idStr == "" {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "No ID entered"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "No ID entered"})
 		return nil
 	}
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success" : false, "message": err.Error()})
+		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success": false, "message": err.Error()})
 		return nil
 	}
-	
+
 	hardware := &models.Hardware{}
 	err = c.BodyParser(hardware)
 	if err != nil {
-		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success" : false, "message": err.Error()})
+		c.Status(http.StatusBadRequest).JSON(&fiber.Map{"success": false, "message": err.Error()})
 		return nil
 	}
 	hardware.ID = uint(id)
@@ -83,42 +121,51 @@ func HardwaresUpdate(c *fiber.Ctx) error {
 	hardwareOrig := &models.Hardware{}
 	res := store.DB.First(hardwareOrig, id)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "Record not found"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "Record not found"})
 		return nil
 	}
 
 	res = store.DB.Save(hardware)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": res.Error.Error()})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": res.Error.Error()})
 		return nil
 	}
-	
-	c.Status(http.StatusOK).JSON(&fiber.Map{"success" : true, "message": "Updated", "hardware": hardware})
+
+	c.Status(http.StatusOK).JSON(&fiber.Map{"success": true, "message": "Updated", "hardware": hardware})
 
 	return nil
 }
 
+// @Summary Delete hardware by ID
+// @Description Delete hardware by ID
+// @Tags Hardwares
+// @Accept json
+// @Produce json
+// @Param id path int true "Hardware ID"
+// @Success 200 {object} fiber.Map
+// @Failure 500 {object} fiber.Map
+// @Router /hardwares/{id} [delete]
 func HardwaresDelete(c *fiber.Ctx) error {
-	id := c.Params("id");
+	id := c.Params("id")
 	if id == "" {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "No ID entered"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "No ID entered"})
 		return nil
 	}
-	
+
 	hardwareOrig := &models.Hardware{}
 	res := store.DB.First(hardwareOrig, id)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": "Record not found"})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": "Record not found"})
 		return nil
 	}
-	
+
 	res = store.DB.Delete(&models.Hardware{}, id)
 	if res.Error != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success" : false, "message": res.Error.Error()})
+		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"success": false, "message": res.Error.Error()})
 		return nil
 	}
-	
-	c.Status(http.StatusOK).JSON(&fiber.Map{"success" : true, "message": "Deleted", "hardware ID": id})
+
+	c.Status(http.StatusOK).JSON(&fiber.Map{"success": true, "message": "Deleted", "hardware ID": id})
 
 	return nil
 }
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 	"github.com/voidmaindev/doctra_lis_middleware/controllers"
+	_ "github.com/voidmaindev/doctra_lis_middleware/docs"
 	"github.com/voidmaindev/doctra_lis_middleware/inits"
 	"github.com/voidmaindev/doctra_lis_middleware/store"
 	"github.com/voidmaindev/doctra_lis_middleware/websockets"
@@ -20,14 +23,32 @@ func init() {
 	store.ConnectToDB()
 }
 
+// @title           Doctra Middleware API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      localhost:9000
+// @BasePath  /
+// @securityDefinitions.basic  BasicAuth
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/hardwares", controllers.HardwaresGetAll)
 	app.Get("/hardwares/:id", controllers.HardwaresGetByID)
 	app.Post("/hardwares", controllers.HardwaresCreate)
 	app.Put("/hardwares/:id", controllers.HardwaresUpdate)
 	app.Delete("/hardwares/:id", controllers.HardwaresDelete)
+
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
 	app.Use("/", websockets.HandleWSUpgradeMiddleware)
 	app.Get("/", websocket.New(websockets.HandleWS))
